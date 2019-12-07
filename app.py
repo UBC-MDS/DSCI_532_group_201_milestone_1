2ac18c750099f39cad2cbbe734591f42ad830df9
Karanpal Singh <karanpal.singh@alumni.ubc.ca>
2019-12-07 12:56:08 -0800

    Plots Description Added on Dash App

--- app.py
+++ app.py
@@ -7,11 +7,20 @@ import dash_html_components as html
 body = dbc.Container(
 
     [   dbc.Row([html.H2("Impact of Disasters on Human Lives", style={'font-family':'Helvetica','color':'darkblue'},className  = "mt-4")]),
+        dbc.Row([
+        
+                dbc.ListGroup(
+                [
+                    dbc.ListGroupItem("Plot 1 (top left): This plot shows the average number of deaths per event of different natural disasters. Since earthquake is the highest, we choose it as the primary target of investigation."),
+                    dbc.ListGroupItem("Plot 2 (top right): This plot shows the trend of annual death rate due to earthquakes from 1990 to 2017 on a worldwide scale."),
+                    dbc.ListGroupItem("Plot 3 (bottom): This plot shows the impact that earthquakes have had on different countries over the years. To see the historical trend for a given country, simply select it on the map. Only one country can be selected at a time. NOTE: There is no default country or year."),
+                ], className="mt-1")
+        
+        ]),
         dbc.Row(
             [
                 dbc.Col(
                     [
-                        
                         html.Iframe(
                                           sandbox='allow-scripts',
                                           id='plot_a',
